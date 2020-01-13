30f942b7697fd97cc3ac960bae09dc62631f8e44
github username <github email address>
2020-01-13 18:29:12 -0500

    Created movies controller with crud actions adding ratings, destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,43 @@ class MoviesController < ApplicationController
 
     render json: @movies
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+
+    render json: @movie
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.user_id = current_user.id
+    if @movie.save
+      render json: @movie
+    else
+        @movie.build_genre #Build/another word for new object
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @note.update(movie_params)
+
+      render json: @movie
+  end
+
+  def ratings
+    @movies = Movie.grouped_ratings #gives access to scope method
+  end
+
+  def destroy
+    @movie.destroy
+
+    render json: @movie 
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :description, :genre, :rating)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create([
-  {title:"Back To The Future", description:"Marty McFly (Michael J. Fox) is thrown back into the '50s when an experiment by his scientist friend Doc Brown (Christopher Lloyd) goes awry.", genre:SciFi}
-  {title:"Star Wars Episode I – The Phantom Menace", description:"Obi-Wan Kenobi is a young apprentice Jedi knight under the tutelage of Qui-Gon Jinn  Anakin Skywalker, who will later father Luke Skywalker and become known as Darth Vader", genre:Scifi}
-  {title:"Rocky", description:"Rocky Balboa, a small-time boxer from working-class Philadelphia, is arbitrarily chosen to take on the reigning world heavyweight champion, Apollo Creed, when the undefeated fighter's scheduled opponent is injured.", genre:Action}
-  {title:"Invincible", description:"Lifelong football fan Vince Papale sees his wildest dreams come true when he becomes a member of the Philadelphia Eagles.", genre:Action}
-  {title:"Top Gun", description:"When hotshot fighter pilot Maverick is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots", genre:Drama}
-  {title:"The Luck of The Irish", description:"A teenager must battle for a gold charm to keep his family from being controlled by an evil leprechaun.", genre:Comedy}
-  {title:"LongShot", description:"When Fred unexpectedly runs into Charlotte, he soon realizes that she was his former baby sitter and childhood crush.", genre:Comedy}
-  ])
