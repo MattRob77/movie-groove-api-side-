1db6d2891e503dbea1a8420c6d8c2bef7b41868c
github username <github email address>
2020-01-15 20:55:06 -0500

    Created some crud actions index, show, create for Reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+
+  def index
+    if @movie = Movie.find_by_id(params[:movieid])
+      @reviews = @movie.reviews.all
+    else
+      render json: @review
+  end
+
+  def show
+    @review = Review.find_by_id(params[:id])
+
+    render json:@review
+  end
+
+  def create
+    @review.Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      render json:@review
+    else
+      @review :new
+  end
+
+  def update
+
+  end 
 end
